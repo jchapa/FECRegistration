ab30b02778ccdccad59f80dbe88525e41ee75210
Benjamin Storrie <brstorrie@gmail.com>
2012-10-13 18:14:26 -0600

    Fixed transition back to first page.
    Added Referral Code
    
    Implemented contact info on second page transferring.

--- Web/index.php
+++ Web/index.php
@@ -16,44 +16,16 @@ $aPersonalFormSessionData = GetFormSessionData($strPersonalFormKey);
 
 ?>
 
-<!DOCTYPE html>
-<html dir="ltr" lang="en-US">
-<head>
-<meta charset="UTF-8" />
+<?php
 
-<title>Register for the Conference! | Family Economics</title>
-	
-<link rel="profile" href="http://gmpg.org/xfn/11" />
+//Header Include
+require_once (dirname(__FILE__) . "/inc/header.inc");
 
+//Begin Body
 
-<link rel="stylesheet" type="text/css" media="all" href="styles/mainstyles.css" />
-<link rel="icon" href="http://www.familyeconomics.com/wp-content/themes/family-economics/images/favicon.png" type="image/png">
-
-<script src="http://www.familyeconomics.com/wp-content/themes/family-economics/js/jquery.js" type="text/javascript"></script>
-<script src="http://www.familyeconomics.com/wp-content/themes/family-economics/js/main.js" type="text/javascript"></script>
-<script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.9/jquery.validate.min.js"></script>
-<script src="http://malsup.github.com/jquery.form.js"></script>
-<script src="scripts/jquery.easing.1.3.js"></script>
-<script src="scripts/stickysidebar.jquery.min.js"></script>
-
-<link rel="pingback" href="http://www.familyeconomics.com/xmlrpc.php" />
-
-<meta name='robots' content='noindex,nofollow' />
-<link rel="alternate" type="application/rss+xml" title="Family Economics &raquo; Feed" href="http://www.familyeconomics.com/feed/" />
-<link rel="alternate" type="application/rss+xml" title="Family Economics &raquo; Comments Feed" href="http://www.familyeconomics.com/comments/feed/" />
-<link rel='stylesheet' id='admin-bar-css'  href='http://www.familyeconomics.com/wp-includes/css/admin-bar.css?ver=3.4.2' type='text/css' media='all' />
-<link rel="EditURI" type="application/rsd+xml" title="RSD" href="http://www.familyeconomics.com/xmlrpc.php?rsd" />
-<link rel="wlwmanifest" type="application/wlwmanifest+xml" href="http://www.familyeconomics.com/wp-includes/wlwmanifest.xml" /> 
-<link rel='prev' title='Speakers' href='http://www.familyeconomics.com/speakers/' />
-<link rel='next' title='Schedule' href='http://www.familyeconomics.com/schedule/' />
-<meta name="generator" content="WordPress 3.4.2" />
-<link rel='canonical' href='http://www.familyeconomics.com/facility-accommodations/' />
-
+?>
 <script type="text/javascript" src="scripts/functions.js"></script>
 
-</head>
-
-<body>
 <?php 
 if ($bDebug)
 {
@@ -481,116 +453,11 @@ if (isset($aPersonalFormSessionData["registration-type"]))
 
 </div><!--End #footer-->
 
-<div id="signup-form">
- 
-<div id="form-container">  
-  
-<form action="http://generationswithvision.us2.list-manage1.com/subscribe/post" method="POST">
-<input type="hidden" name="u" value="641279d927137eec6730e767f">
-<input type="hidden" name="id" value="ae104eb7d4">
+<?php
 
+//Header Include
+require_once (dirname(__FILE__) . "/inc/footer.inc");
 
-<div id="mergeTable" class="mergeTable">
+//Begin Body
 
-<ul id="form">  
-
-<li>
-<div class="mergeRow dojoDndItem mergeRow-email" id="mergeRow-0">
-
-<label for="MERGE0"><strong>Email Address</strong> <span class="asterisk">*</span></label>
-<div class="field-group">
-    <input type="email" autocapitalize="off" autocorrect="off" name="MERGE0" id="MERGE0" size="25" value="">
-</div><!--End .field-group-->
-</div><!--End #mergeRow-->
-  </li>
-
-<li>
-<div class="mergeRow dojoDndItem mergeRow-text" id="mergeRow-1">
-<label for="MERGE1">First Name</label>
-<div class="field-group">
-    <input type="text" name="MERGE1" id="MERGE1" size="25" value="">
-</div><!--End .field-group-->
-</div><!--End #mergeRow-->
-  </li>
-
-<li>
-<div class="mergeRow dojoDndItem mergeRow-text" id="mergeRow-2">
-<label for="MERGE2">Last Name</label>
-<div class="field-group">
-    <input type="text" name="MERGE2" id="MERGE2" size="25" value="">
-</div><!--End .field-group-->
-</div><!--End #mergeRow-->
-  </li>
-
-
-<li>
-<div class="mergeRow dojoDndItem mergeRow-text" id="mergeRow-4">
-<label for="MERGE4">Address</label>
-<div class="field-group">
-    <input type="text" name="MERGE4" id="MERGE4" size="25" value="">
-</div><!--End .field-group-->
-</div><!--End #mergeRow-->
-  </li>
-
-<li>
-<div class="mergeRow dojoDndItem mergeRow-text" id="mergeRow-3">
-<label for="MERGE3">City</label>
-<div class="field-group">
-    <input type="text" name="MERGE3" id="MERGE3" size="25" value="">
-</div><!--End .field-group-->
-</div><!--End #mergeRow-->
-  </li>
-
-<li>
-<div class="mergeRow dojoDndItem mergeRow-text" id="mergeRow-6">
-<label for="MERGE6">State</label>
-<div class="field-group">
-    <input type="text" name="MERGE6" id="MERGE6" size="25" value="">
-</div><!--End .field-group-->
-</div><!--End #mergeRow-->
-  </li>
-  
-  
-<li>
-<div class="mergeRow dojoDndItem mergeRow-text" id="mergeRow-7">
-<label for="MERGE7">Zip</label>
-<div class="field-group">
-    <input type="text" name="MERGE7" id="MERGE7" size="25" value="">
-</div><!--End .field-group-->
-</div><!--End #mergeRow-->
- </li>
-  
-  
-<li>
-<div id="interestTable">
-    
-<div id="mergeRow-100-7393" class="mergeRow dojoDndItem mergeRowHidden mergeRow-interests-hidden" style="display:none;">
-			<label>Referred by</label>
-	        <div class="field-group groups">
-<ul class="interestgroup_field" id="interestgroup_field_455445"><li class="interestgroup_row"><input type="checkbox" id="group_1" name="group[7393][1]" value="1">&nbsp;<label for="group_1">hslda</label></li><li class="interestgroup_row"><input type="checkbox" id="group_2" name="group[7393][2]" value="1">&nbsp;<label for="group_2">wnd</label></li></ul>
-            </div><!--End .field-group-->
-
-</div><!--End #mergeRow-100-7393-->
-</div><!--End #interestTable-->
-  </li>    
-
-<br>
-
-
-<div class="submit_container">
-<input type="submit" class="button" name="submit" value="Subscribe to list">
-</div><!--End #submit_container-->
- 
-</div><!--End #mergeTable-->
-</ul><!--End #form-->
-
-</form>
-
-
-</div><!--End #form-container-->
-</div><!--End #signup-form-->
-
-
-		
-</body>
-</html>
+?>
--- Web/scripts/payment-functions.js
+++ Web/scripts/payment-functions.js
@@ -1,12 +1,67 @@
 // JavaScript Document
 $(document).ready(function() {
     // Payment page Scripts
+		  				$("#same-as-contact").attr('checked', true);
+							$(".contact-duplicate").attr('disabled', true);
+														
+		                    var validate = $("#billing-info")
+                            .validate(
+                                    {
+                                        debug : false,
+                                        rules : {
+																					"card-name" : {
+																						required: true,
+																						minlength: 2
+																					},
+                                          "card-number" : {
+																						required: true,
+																						digits: true,
+																						maxlength: 30
+																					},
+																					year : {
+																						required: true,
+																						minlength: 4,
+																						digits: true
+																					},
+																					csc : {
+																						required: true,
+																						minlength: 3,
+																						digits: true
+																					}
+                                        },
+                                        messages : {
+																					"card-name" : {
+																						required: "Name on Card is Required",
+																						minlength: "Name on Card must be 2 digits"
+																					},
+                                          "card-number" :  {
+																						required: "Card Number is Required",
+																						digits: "Card Number must be digits."
+																					},
+																					year : {
+																					  required: "Expiration Year is required",
+																						minlength: "Expiration Year must be 4 digits",
+																						digits: "Expiration Year must be digits."
+																					},
+																					csc : {
+																						required: "Security Code is required",
+																						minlength: "Security Code must be at least 3 characters",
+																						digits: "Security Code must be digits"
+																					}
+                                        },
+                                        errorContainer : "#error-container",
+                                        errorLabelContainer : "#error-label-container"
+                                    });
+																		
+                    $("error-container").hide();
+                    $("#error-container").stickySidebar();
 		
 		$("#same-as-contact").click(function() {
 					if (this.checked) {
-						$(".contact-duplicate").attr('disabled', true);
+						//$(".contact-duplicate").attr('disabled', true);
 					} else {
 						$(".contact-duplicate").removeAttr("disabled");		
+						$("#same-as-contact-li").fadeOut('slow');
 					}
 		});
     		
